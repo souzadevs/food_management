01be0067df64992653668ee35477750652c0a9b3
Bruno <eletronica.bruno@hotmail.com>
2023-03-22 15:43:56 -0300

    Start Food Management project

---
+++ database/migrations/2023_03_22_181847_create_roles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string("description");
+            $table->unsignedBigInteger("profile_id");
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table
+                ->foreign("profile_id")
+                ->references("id")
+                ->on("profiles");
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
---
+++ database/migrations/2023_03_22_183645_create_profile_role_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_role', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("profile_id");
+            $table->unsignedBigInteger("role_id");
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign("profile_id")->references("id")->on("profiles");
+            $table->foreign("role_id")->references("id")->on("roles");
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_role');
+    }
+}
